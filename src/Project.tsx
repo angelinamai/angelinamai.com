ca87bfd880a8e33589b26f0d9861a46d4f7eb8ab
Angelina Mai <angelinaxu9@gmail.com>
2023-12-27 15:20:54 -0500

    new update

--- src/Project.tsx
+++ src/Project.tsx
@@ -5,7 +5,10 @@ function Project({ name, img, description, link }: any) {
       className="bg-pink-100 p-5 rounded-md hover:bg-slate-800 hover:text-white transition-all duration-500"
     >
       <div className="text-2xl">{name}</div>
-      <img className="rounded-md" src={img} />
+      <div
+        className="rounded-md bg-cover h-44 min-w-full border-2 border-pink-200"
+        style={{ backgroundImage: `url("${img}")` }}
+      />
       <div className="italic">{description}</div>
     </a>
   );
--- src/myprojects.ts
+++ src/myprojects.ts
@@ -1,25 +1,29 @@
 const myProjects = [
   {
-    name: "Project 1",
-    description: "Test for now",
-    img: "https://hips.hearstapps.com/hmg-prod/images/dog-puppy-on-garden-royalty-free-image-1586966191.jpg?crop=0.752xw:1.00xh;0.175xw,0&resize=1200:*",
+    name: "Eat and Split",
+    description: "Let's share the bill every time we eat together",
+    img: "/images/eatnsplit.png",
     link: "/projects/eatnsplit/index.html"
   },
   {
-    name: "Project 2",
-    description: "Test for later",
-    img: "https://hips.hearstapps.com/hmg-prod/images/dog-puppy-on-garden-royalty-free-image-1586966191.jpg?crop=0.752xw:1.00xh;0.175xw,0&resize=1200:*",
+    name: "Travel List",
+    description: "Tired of forgetting things when traveling? Let's use this app",
+    img: "/images/travellist.png",
+    link: "/projects/travellist/index.html"
   },
   {
-    name: "Project 3",
-    description: "Test for future",
-    img: "https://hips.hearstapps.com/hmg-prod/images/dog-puppy-on-garden-royalty-free-image-1586966191.jpg?crop=0.752xw:1.00xh;0.175xw,0&resize=1200:*",
+    name: "Pizza Menu",
+    description: "Welcome to our restaurant with different kind of pizzas that you are craving for? ",
+    img: "/images/pizzamenufinal.png",
+    link: "/projects/pizzamenufinal/index.html"
   },
   {
-    name: "Project 4",
-    description: "Test for s",
+    name: "Weather App",
+    description: "It's sunny today, wanna go for a walk?",
     img: "https://hips.hearstapps.com/hmg-prod/images/dog-puppy-on-garden-royalty-free-image-1586966191.jpg?crop=0.752xw:1.00xh;0.175xw,0&resize=1200:*",
+    link: "/projects/weatherapp/index.html"
   },
+
 
 
 
