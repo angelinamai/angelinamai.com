d2523617f598b1ee9fc6fa62ad72146ea4638641
Angelina Mai <angelinaxu9@gmail.com>
2024-05-24 18:22:12 -0400

    hovercolorchange

--- src/Project.tsx
+++ src/Project.tsx
@@ -2,7 +2,7 @@ function Project({ name, img, description, link }: any) {
   return (
     <a
       href={link}
-      className="bg-pink-100 p-5 rounded-md hover:bg-slate-800 hover:text-white transition-all duration-500"
+      className="bg-pink-100 p-5 rounded-md hover:bg-gray-100 hover:text-white transition-all duration-500"
     >
       <div className="text-2xl">{name}</div>
       <div
